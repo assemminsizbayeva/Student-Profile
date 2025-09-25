9adcd0cb417cd3678c35cc4502b7248fdd488d23
Assem Minsizbayeva <assem.minsizbayeva_2028@ucentralasia.org>
2025-09-25 23:12:54 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
+
 document.querySelector('.contact-form').addEventListener('submit', function(e) {
     e.preventDefault();
     alert('Thank you for your message, Assem will get back to you soon!');
     this.reset();
+});
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
